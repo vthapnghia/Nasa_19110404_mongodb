497d2b8de863cf96be3d6192dff42af68abe2536
vthapnghia <103355716+vthapnghia@users.noreply.github.com>
2022-11-21 15:36:58 +0700

    Initial commit

---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,67 @@
+const {
+  getAllLaunches,
+  scheduleNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
+
+const { getPagination } = require("../../services/query");
+
+async function httpGetALLLaunch(req, res) {
+  const { skip, limit } = getPagination(req.query);
+  const launches = await getAllLaunches(skip, limit);
+  return res.status(200).json(launches);
+}
+
+async function httpAddNewLauch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
+  await scheduleNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
+async function httpAbortLaunch(req, res) {
+  const launchID = Number(req.params.id);
+
+  const existsLaunch = await existsLaunchWithId(launchID);
+  if (!existsLaunch) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  const aborted = await abortLaunchById(launchID);
+  if (!aborted) {
+    return res.status(400).json({
+      error: "Launch not aborted",
+    });
+  }
+
+  return res.status(200).json({
+    oke: true,
+  });
+}
+
+module.exports = {
+  httpGetALLLaunch,
+  httpAddNewLauch,
+  httpAbortLaunch,
+};
